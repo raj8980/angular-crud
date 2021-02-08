c0f954d8ebddd9a827485caf2cc5399e381b8372
raj8980 <rajpatel8980.rp42@gmail.com>
2021-02-08 13:10:08 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
+import { AboutUsComponent } from './components/about-us/about-us.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { AdminGuard } from './admin/admin.guard';
+import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';
+import { ServerErrorComponent } from './components/server-error/server-error.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'about-us',component:AboutUsComponent
+  },
+  {
+    path:'login',component:LoginComponent
+  },
+  {
+    path:'logout',component:LogoutComponent
+  },
+  {
+    path:'unauthorized-access',component:UnauthorizedComponent
+  },
+  {
+    path:'server-error',component:ServerErrorComponent
+  },
+  { 
+    path: 'users', 
+    loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
+    canActivate:[AdminGuard]
+  },
+  {
+    path:'**',component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
